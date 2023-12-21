7b4a90e595411685e5dfa095b2f54dd2b40ac921
Peppy3-biz <unhappymilk7@gmail.com>
2023-12-21 16:37:33 +0100

    Update downloadJSON.py

--- TinderBot/TextingFunctionality/downloadJSON.py
+++ TinderBot/TextingFunctionality/downloadJSON.py
@@ -7,6 +7,13 @@ for window in gw.getAllTitles():
     if "Tinder | Dating," in window:
         window_name = window
 
+def click(image):
+    thingLocation = pyautogui.locateOnScreen(image)
+    if thingLocation:
+        pyautogui.click(thingLocation)
+    else:
+        return 'not found'
+
 # Name of the window you want to focus
 #window_name = "Tinder | Dating, Make Friends & Meet New People and 7 more pages - Personal - Microsoft​ Edge"
 
@@ -25,14 +32,11 @@ if window is not None:
 
 
 networkImage = r"C:\Users\Unhap\OneDrive\Documents\GitHub\Tinder\TinderBot\TextingFunctionality\networkTab.png"
-networkTabLocation = pyautogui.locateOnScreen(networkImage)
-if networkTabLocation:
-    pyautogui.click(networkTabLocation)
-else:
+if click(networkImage) == 'not found':
     pyautogui.hotkey('ctrl', 'shift', 'i')
     sleep(1)
-    if networkTabLocation:
-        pyautogui.click(networkTabLocation)
+    if click(networkImage):
+        print("Error: Can't find Network Tab")
 
 # Refresh the page to get Network Data
 pyautogui.press('f5')
